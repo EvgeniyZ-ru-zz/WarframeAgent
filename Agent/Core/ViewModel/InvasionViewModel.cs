8b1ca5ce22931784ea07fe243c8a0cb2389f88ea
GodMar <stepsis2009@live.com>
2017-08-06 18:42:19 +0300

    Исправления в дизайне вторжений

--- Agent/Core/ViewModel/InvasionViewModel.cs
+++ Agent/Core/ViewModel/InvasionViewModel.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows;
+using System.Windows.Media;
 using Core.Model;
 
 namespace Core.ViewModel
@@ -19,8 +20,15 @@ namespace Core.ViewModel
             isDefenderFactionInfestation = invasion.DefenderMissionInfo.Faction == "FC_INFESTATION";
             AttackerFaction = invasion.AttackerMissionInfo.Faction.GetFilter(Model.Filters.FilterType.Fraction).FirstOrDefault().Key;
             DefenderFaction = invasion.DefenderMissionInfo.Faction.GetFilter(Model.Filters.FilterType.Fraction).FirstOrDefault().Key;
+            AttackerFactionDefault = invasion.AttackerMissionInfo.Faction;
+            DefenderFactionDefault = invasion.DefenderMissionInfo.Faction;
             Faction = invasion.Faction.GetFilter(Model.Filters.FilterType.Fraction).FirstOrDefault().Key;
             NodeArray = invasion.Node.GetFilter(Model.Filters.FilterType.Planet).FirstOrDefault().Key.ToUpper().Split('|');
+            LocTag = invasion.LocTag.GetFilter(Model.Filters.FilterType.Mission).FirstOrDefault().Key;
+            DefenderReward = invasion.DefenderReward?.CountedItems[0]?.ItemType.GetFilter(Model.Filters.FilterType.Item).FirstOrDefault().Key;
+            DefenderRewardCount = invasion.DefenderReward?.CountedItems[0]?.ItemCount;
+            //AttackerReward = invasion.AttackerReward?.CountedItems[0]?.ItemType.GetFilter(Model.Filters.FilterType.Item).FirstOrDefault().Key;
+            //AttackerRewardCount = invasion.AttackerReward?.CountedItems[0]?.ItemCount;
             Update();
         }
 
@@ -29,13 +37,24 @@ namespace Core.ViewModel
             Count = invasion.Count;
             Goal = invasion.Goal;
             UpdatePercent();
+            UpdateColor();
+            UpdateReward();
         }
 
         public Id Id { get; }
+        public string LocTag { get; }
         public string Faction { get; }
         public string[] NodeArray { get; }
         public string DefenderFaction { get; }
         public string AttackerFaction { get; }
+        public string DefenderFactionDefault { get; }
+        public string AttackerFactionDefault { get; }
+        public Brush DefenderColor { get; set; }
+        public Brush AttackerColor { get; set; }
+        public string DefenderReward { get; set; }
+        public string AttackerReward { get; set; }
+        public int? DefenderRewardCount { get; }
+        public int? AttackerRewardCount { get; }
 
         private readonly bool isDefenderFactionInfestation;
 
@@ -61,6 +80,49 @@ namespace Core.ViewModel
 
             Percent = val;
             PercentOut = 100 - val;
+        }
+
+        void UpdateColor()
+        {
+            switch (DefenderFactionDefault)
+            {
+                case "FC_GRINEER":
+                    DefenderColor = new SolidColorBrush(Color.FromRgb(r: 0xbd, g: 0x58, b: 0x57)); //FFbd5857
+                    break;
+                case "FC_CORPUS":
+                    DefenderColor = new SolidColorBrush(Color.FromRgb(r: 0x3f, g: 0x85, b: 0xca)); //FF3f85ca
+                    break;
+                case "FC_INFESTATION":
+                    DefenderColor = new SolidColorBrush(Color.FromRgb(r: 0x1f, g: 0x97, b: 0x45)); //FF1F9745
+                    break;
+            }
+
+            switch (AttackerFactionDefault)
+            {
+                case "FC_GRINEER":
+                    AttackerColor = new SolidColorBrush(Color.FromRgb(r: 0xbd, g: 0x58, b: 0x57)); //FFbd5857
+                    break;
+                case "FC_CORPUS":
+                    AttackerColor = new SolidColorBrush(Color.FromRgb(r: 0x3f, g: 0x85, b: 0xca)); //FF3f85ca
+                    break;
+                case "FC_INFESTATION":
+                    AttackerColor = new SolidColorBrush(Color.FromRgb(r: 0x1f, g: 0x97, b: 0x45)); //FF1F9745
+                    break;
+            }
+        }
+
+        void UpdateReward()
+        {
+            if (DefenderReward != null)
+            {
+                if (DefenderRewardCount != null && DefenderRewardCount > 1)
+                    DefenderReward += $" [{DefenderRewardCount}]";
+            }
+            if (AttackerReward != null)
+            {
+                if (AttackerRewardCount != null && AttackerRewardCount > 1)
+                    AttackerReward += $" [{AttackerRewardCount}]";
+            }
         }
 
         private double _percentOut;
