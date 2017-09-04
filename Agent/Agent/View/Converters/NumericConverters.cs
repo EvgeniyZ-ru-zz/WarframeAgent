308b1ee8914c6b4969de305cd6503947e58a164b
VladD <VladD@TIGGER>
2017-09-04 23:43:25 +0200

    Релизовал обратный отсчёт для #50

---
+++ Agent/Agent/View/Converters/NumericConverters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Agent.View.Converters
+{
+    class ConstantMultiplyingLongConverter : IValueConverter
+    {
+        public long Multiplier { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            (long)value * Multiplier;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            (long)value / Multiplier;
+    }
+}
