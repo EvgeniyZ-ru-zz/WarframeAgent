0204be98fb8872a196fa3303194e1f59179bf036
GodMar <stepsis2009@live.com>
2017-12-25 19:26:55 +0300

    Чистка игрового времени.

--- Agent/Core/ViewModel/CetusTimeViewModel.cs
+++ Agent/Core/ViewModel/CetusTimeViewModel.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Windows.Media;
-using System.Windows.Threading;
-using Core.Model;
 
 namespace Core.ViewModel
 {
@@ -9,7 +6,7 @@ namespace Core.ViewModel
     {
         public CetusTimeViewModel() => UpdateTime();
 
-        DateTime time = new DateTime(2017, 12, 20);
+        private DateTime time;
         public DateTime Time
         {
             get => time;
@@ -48,159 +45,58 @@ namespace Core.ViewModel
 
         public void UpdateTime()
         {
+            #region JS Code
 
-            //            function updateTime() {
-            //    $(".time").each(function(index) {
-            //        var starttime = moment.unix($(this).data("starttime"));
-            //        var endtime = moment.unix($(this).data("endtime"));
-            //        var timetext = "";
-            //        if (moment() < starttime) {
-            //            var duration = moment.duration(starttime.diff(moment(), 'seconds'), 'seconds');
-            //            if (duration.days() != 0)
-            //                timetext = "Start: " + pad2(duration.days()) + "d " + pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            else if (duration.hours() != 0)
-            //                timetext = "Start: " + pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            else
-            //                timetext = "Start: " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            $(this).css("background-color", "#999");
-            //        } else if (starttime < moment()) {
-            //            var duration = moment.duration(endtime.diff(moment(), 'seconds'), 'seconds');
-            //            if (duration.days() != 0)
-            //                timetext = pad2(duration.days()) + "d " + pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            else
-            //                timetext = pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            $(this).css("background-color", "green");
-            //            if (endtime < moment()) {
-            //                $(this).css("background-color", "red");
-            //                timetext = "Expired: " + timetext;
+            //function updateCetus() {
+            //    var color = 'black';
+            //    var timeLeft = 'Loading';
+            //    var cycle = '???';
+            //    if (syndicateData === undefined) {
+            //        pullSyndicates();
+            //    } else {
+            //        for (var i = 0; i < syndicateData.length; i++) {
+            //            if (syndicateData[i].Tag != 'CetusSyndicate')
+            //                continue;
+            //            var activation = syndicateData[i].Activation.sec * 1000 + syndicateData[i].Activation.usec;
+            //            var expiry = syndicateData[i].Expiry.sec * 1000 + syndicateData[i].Expiry.usec;
+            //            var now = moment().valueOf();
+            //            if (now > expiry) {
+            //                pullSyndicates();
+            //                break;
             //            }
-            //        }
-            //        $(this).html("<span title=\"Start: " + starttime.format("dddd, MMMM Do YYYY, h:mm:ss a") + " End: " + endtime.format("dddd, MMMM Do YYYY, h:mm:ss a") + "\">" + timetext + "</span>");
-            //    });
-            //    $(".vttime").each(function(index) {
-            //        var starttime = moment.unix($(this).data("starttime"));
-            //        var endtime = moment.unix($(this).data("endtime"));
-            //        var timetext = "";
-            //        if (moment() < starttime) {
-            //            var duration = moment.duration(starttime.diff(moment(), 'seconds'), 'seconds');
-            //            if (duration.days() != 0)
-            //                timetext = "Start: " + pad2(duration.days()) + "d " + pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            else if (duration.hours() != 0)
-            //                timetext = "Start: " + pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            else
-            //                timetext = "Start: " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //        } else if (starttime < moment()) {
-            //            var duration = moment.duration(endtime.diff(moment(), 'seconds'), 'seconds');
-            //            if (duration.days() != 0)
-            //                timetext = pad2(duration.days()) + "d " + pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            else
-            //                timetext = pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            $(this).css("color", "green");
-            //            if (endtime < moment()) {
-            //                timetext = "Expired: " + timetext;
-            //                $(this).css("color", "red");
+            //            if (expiry - now > 0 && expiry - now < 30000) {
+            //                pullSyndicates();
             //            }
+            //            if (now > activation && now < expiry) {
+            //                var timeSinceDusk = now - activation;
+            //                if (timeSinceDusk < 100 * 60 * 1000) {
+            //                    color = 'orange';
+            //                    cycle = 'Day';
+            //                    timeLeft = 'Time left: ' + formatCetus((100 * 60 * 1000 - timeSinceDusk) / 1000);
+            //                } else {
+            //                    color = 'darkblue';
+            //                    cycle = 'Night';
+            //                    timeLeft = 'Time left: ' + formatCetus((150 * 60 * 1000 - timeSinceDusk) / 1000);
+            //                }
+            //            }
+            //            break;
             //        }
-            //        $(this).html("<span title=\"Start: " + starttime.format("dddd, MMMM Do YYYY, h:mm:ss a") + " End: " + endtime.format("dddd, MMMM Do YYYY, h:mm:ss a") + "\">" + timetext + "</span>");
-            //    });
-            //    $(".bltime").each(function(index) {
-            //        var endtime = moment.unix($(this).data("endtime"));
-            //        var timetext = "Time remaining: ";
-            //        if (endtime > moment()) {
-            //            var duration = moment.duration(endtime.diff(moment(), 'seconds'), 'seconds');
-            //            timetext += pad2(duration.hours() + duration.days() * 24) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //        } else if (endtime < moment()) {
-            //            var duration = moment.duration(endtime.diff(moment(), 'seconds'), 'seconds');
-            //            timetext += "-" + pad2(duration.hours() + duration.days() * -24) + "h " + pad2(duration.minutes() * -1) + "m " + pad2(duration.seconds() * -1) + "s";
-            //        }
-            //        $(this).html("<span title=\"End: " + endtime.format("dddd, MMMM Do YYYY, h:mm:ss a") + "\">" + timetext + "</span>");
-            //    });
-            //    $(".sortietime").each(function(index) {
-            //        var endtime = moment.unix($(this).data("endtime"));
-            //        var timetext = "";
-            //        if (endtime > moment()) {
-            //            var duration = moment.duration(endtime.diff(moment(), 'seconds'), 'seconds');
-            //            timetext += pad2(duration.hours() + duration.days() * 24) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //        } else if (endtime < moment()) {
-            //            var duration = moment.duration(endtime.diff(moment(), 'seconds'), 'seconds');
-            //            timetext += "-" + pad2(duration.hours() + duration.days() * -24) + "h " + pad2(duration.minutes() * -1) + "m " + pad2(duration.seconds() * -1) + "s";
-            //        }
-            //        $(this).html("<span title=\"End: " + endtime.format("dddd, MMMM Do YYYY, h:mm:ss a") + "\">" + timetext + "</span>");
-            //    });
-            //    $(".bldeploytime").each(function(index) {
-            //        var endtime = moment.unix($(this).data("time"));
-            //        var timetext = "Time to deployment: ";
-            //        if (endtime > moment()) {
-            //            var duration = moment.duration(endtime.diff(moment(), 'seconds'), 'seconds');
-            //            timetext += pad2(duration.hours() + duration.days() * 24) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //        } else if (endtime < moment()) {
-            //            var duration = moment.duration(endtime.diff(moment(), 'seconds'), 'seconds');
-            //            timetext += "-" + pad2(duration.hours() + duration.days() * -24) + "h " + pad2(duration.minutes() * -1) + "m " + pad2(duration.seconds() * -1) + "s";
-            //        }
-            //        $(this).html("<span title=\"Deployment: " + endtime.format("dddd, MMMM Do YYYY, h:mm:ss a") + "\">" + timetext + "</span>");
-            //    });
-            //    $(".taxtime").each(function(index) {
-            //        var endtime = moment.unix($(this).data("time"));
-            //        var timetext = "Allowed in: ";
-            //        if (endtime > moment()) {
-            //            var duration = moment.duration(endtime.diff(moment(), 'seconds'), 'seconds');
-            //            timetext += pad2(duration.hours() + duration.days() * 24) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //        } else if (endtime < moment()) {
-            //            timetext = "Tribute change allowed.";
-            //        }
-            //        $(this).html("<span title=\"Allowed: " + endtime.format("dddd, MMMM Do YYYY, h:mm:ss a") + "\">" + timetext + "</span>");
-            //    });
-            //    $(".header-time").each(function(index) {
-            //        if ($(this).data("time") == '0')
-            //            return;
-            //        var endtime = moment.unix($(this).data("time"));
-            //        var timetext = "";
-            //        if (endtime > moment()) {
-            //            var duration = moment.duration(endtime.diff(moment(), 'seconds'), 'seconds');
-            //            timetext += pad2(duration.hours() + duration.days() * 24) + ":" + pad2(duration.minutes()) + ":" + pad2(duration.seconds()) + ' |';
-            //        } else if (endtime < moment()) {
-            //            timetext = "Completed.";
-            //        }
-            //        $(this).html("<span title=\"" + endtime.format("dddd, MMMM Do YYYY, h:mm:ss a") + "\">" + timetext + "</span>");
-            //    });
-            //    $(".target-time").each(function(index) {
-            //        var starttime = moment.unix($(this).data("starttime"));
-            //        var timetext = "";
-            //        var duration = '';
-            //        if (moment() < starttime) {
-            //            var duration = moment.duration(starttime.diff(moment(), 'seconds'), 'seconds');
-            //            if (duration.days() != 0)
-            //                timetext = "Start: " + pad2(duration.days()) + "d " + pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            else if (duration.hours() != 0)
-            //                timetext = "Start: " + pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            else
-            //                timetext = "Start: " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //        } else if (starttime < moment()) {
-            //            var duration = moment.duration(moment().diff(starttime, 'seconds'), 'seconds');
-            //            if (duration.days() != 0)
-            //                timetext = pad2(duration.days()) + "d " + pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            else
-            //                timetext = pad2(duration.hours()) + "h " + pad2(duration.minutes()) + "m " + pad2(duration.seconds()) + "s";
-            //            $(this).css("background-color", "green");
-            //        }
-            //        $(this).html("<span title=\"" + starttime.format("dddd, MMMM Do YYYY, h:mm:ss a") + "\">" + timetext + "</span>");
-            //    });
-            //    var hour = Math.floor(moment().valueOf() / 3600000) % 24;
-            //    var cycle = 'Night';
-            //    var colour = 'darkblue';
-            //    if ((hour >= 0 && hour < 4) || (hour >= 8 && hour < 12) || (hour >= 16 && hour < 20)) {
-            //        cycle = 'Day';
-            //        colour = 'orange';
             //    }
-            //    var hourleft = 3 - (hour % 4);
-            //    minutes = 59 - moment().minutes();
-            //    seconds = 59 - moment().seconds();
-            //    $('#daynight').text(cycle).css('color', colour);
-            //    $('#daynight-timeleft').text("Time left: " + pad2(hourleft) + "h " + pad2(minutes) + "m " + pad2(seconds) + "s");
-            //    updateCetus();
+            //    $('#cetusdaynight').text(cycle).css('color', color);
+            //    $('#cetusdaynight-timeleft').text(timeLeft);
             //}
 
+            //function formatCetus(t) {
+            //    var hours = Math.floor(t / 3600);
+            //    var minutes = Math.ceil((t - (hours * 3600)) / 60);
+            //    var ret = '';
+            //    if (hours > 0)
+            //        ret = hours + 'h ';
+            //    ret += minutes + 'm';
+            //    return ret;
+            //}
 
+            #endregion
         }
     }
 }
--- Agent/Core/ViewModel/EarthTimeViewModel.cs
+++ Agent/Core/ViewModel/EarthTimeViewModel.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Windows.Media;
-using System.Windows.Threading;
 
 namespace Core.ViewModel
 {
@@ -8,7 +6,7 @@ namespace Core.ViewModel
     {
         public EarthTimeViewModel() => UpdateTime();
 
-        DateTime time = new DateTime(2017, 12, 20);
+        private DateTime time;
         public DateTime Time
         {
             get => time;
@@ -47,63 +45,10 @@ namespace Core.ViewModel
 
         public void UpdateTime()
         {
+            var now = DateTime.UtcNow;
+            var hour = now.Hour;
 
-//function updateCetus() {
-//    var color = 'black';
-//    var timeLeft = 'Loading';
-//    var cycle = '???';
-//    if (syndicateData === undefined) {
-//        pullSyndicates();
-//    } else {
-//        for (var i = 0; i < syndicateData.length; i++) {
-//            if (syndicateData[i].Tag != 'CetusSyndicate')
-//                continue;
-//            var activation = syndicateData[i].Activation.sec * 1000 + syndicateData[i].Activation.usec;
-//            var expiry = syndicateData[i].Expiry.sec * 1000 + syndicateData[i].Expiry.usec;
-//            var now = moment().valueOf();
-//            if (now > expiry) {
-//                pullSyndicates();
-//                break;
-//            }
-//            if (expiry - now > 0 && expiry - now < 30000) {
-//                pullSyndicates();
-//            }
-//            if (now > activation && now < expiry) {
-//                var timeSinceDusk = now - activation;
-//                if (timeSinceDusk < 100 * 60 * 1000) {
-//                    color = 'orange';
-//                    cycle = 'Day';
-//                    timeLeft = 'Time left: ' + formatCetus((100 * 60 * 1000 - timeSinceDusk) / 1000);
-//                } else {
-//                    color = 'darkblue';
-//                    cycle = 'Night';
-//                    timeLeft = 'Time left: ' + formatCetus((150 * 60 * 1000 - timeSinceDusk) / 1000);
-//                }
-//            }
-//            break;
-//        }
-//    }
-//    $('#cetusdaynight').text(cycle).css('color', color);
-//    $('#cetusdaynight-timeleft').text(timeLeft);
-//}
-
-//function formatCetus(t) {
-//    var hours = Math.floor(t / 3600);
-//    var minutes = Math.ceil((t - (hours * 3600)) / 60);
-//    var ret = '';
-//    if (hours > 0)
-//        ret = hours + 'h ';
-//    ret += minutes + 'm';
-//    return ret;
-//}
-
-
-            var now = DateTime.Now;
-
-            var hour = Math.Floor((double)(Tools.Time.ToUnixTime(DateTime.Now) / 3600000)) % 24;
-            var hour1 = now.Hour;
-
-            if ((hour >= 0 && hour < 4) || (hour >= 8 && hour < 12) || (hour >= 16 && hour < 20))
+            if (hour >= 0 && hour < 4 || hour >= 8 && hour < 12 || hour >= 16 && hour < 20)
             {
                 Cycle = "День";
                 CycleIcon = "SunOutline";
@@ -118,11 +63,11 @@ namespace Core.ViewModel
                 ColorTwo = "#4C3782CD";
             }
 
-            var hourleft = 3 - (hour % 4);
+            var hourleft = 3 - hour % 4;
             var minutes = 59 - now.Minute;
             var seconds = 59 - now.Second;
 
-            Time = now.Date + new TimeSpan((int)hourleft, minutes, seconds);
+            Time = now.Date + new TimeSpan(hourleft, minutes, seconds);
         }
     }
 }
