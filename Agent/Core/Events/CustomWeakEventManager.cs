4ede6cf49178725e70882fe64243d6740ccaf3af
GodMar <stepsis2009@live.com>
2017-12-27 19:59:52 +0300

    Ивенты обновления фильтров.

--- Agent/Core/Events/CustomWeakEventManager.cs
+++ Agent/Core/Events/CustomWeakEventManager.cs
@@ -44,14 +44,94 @@ namespace Core.Events
             DeliverEvent(sender, e);
     }
 
+    #region Items 
+
     class ItemsUpdatedWeakEventManager : CustomWeakEventManager<ItemsUpdatedWeakEventManager, EventArgs>
     {
         private ItemsUpdatedWeakEventManager() { }
 
         protected override void StartListening(object source) =>
             ((FiltersEvent)source).ItemsUpdated += OnItemsUpdated;
-        
+
         protected override void StopListening(object source) =>
             ((FiltersEvent)source).ItemsUpdated -= OnItemsUpdated;
     }
+
+    #endregion
+
+    #region Sectors 
+
+    class SectorsUpdatedWeakEventManager : CustomWeakEventManager<SectorsUpdatedWeakEventManager, EventArgs>
+    {
+        private SectorsUpdatedWeakEventManager() { }
+
+        protected override void StartListening(object source) =>
+            ((FiltersEvent)source).SectorsUpdated += OnItemsUpdated;
+
+        protected override void StopListening(object source) =>
+            ((FiltersEvent)source).SectorsUpdated -= OnItemsUpdated;
+    }
+
+    #endregion
+
+    #region Missions 
+
+    class MissionsUpdatedWeakEventManager : CustomWeakEventManager<MissionsUpdatedWeakEventManager, EventArgs>
+    {
+        private MissionsUpdatedWeakEventManager() { }
+
+        protected override void StartListening(object source) =>
+            ((FiltersEvent)source).MissionsUpdated += OnItemsUpdated;
+
+        protected override void StopListening(object source) =>
+            ((FiltersEvent)source).MissionsUpdated -= OnItemsUpdated;
+    }
+
+    #endregion
+
+    #region Factions 
+
+    class FactionsUpdatedWeakEventManager : CustomWeakEventManager<FactionsUpdatedWeakEventManager, EventArgs>
+    {
+        private FactionsUpdatedWeakEventManager() { }
+
+        protected override void StartListening(object source) =>
+            ((FiltersEvent)source).FactionsUpdated += OnItemsUpdated;
+
+        protected override void StopListening(object source) =>
+            ((FiltersEvent)source).FactionsUpdated -= OnItemsUpdated;
+    }
+
+    #endregion
+
+    #region Builds 
+
+    class BuildsUpdatedWeakEventManager : CustomWeakEventManager<BuildsUpdatedWeakEventManager, EventArgs>
+    {
+        private BuildsUpdatedWeakEventManager() { }
+
+        protected override void StartListening(object source) =>
+            ((FiltersEvent)source).BuildsUpdated += OnItemsUpdated;
+
+        protected override void StopListening(object source) =>
+            ((FiltersEvent)source).BuildsUpdated -= OnItemsUpdated;
+    }
+
+    #endregion
+
+    #region Planets 
+
+    class PlanetsUpdatedWeakEventManager : CustomWeakEventManager<PlanetsUpdatedWeakEventManager, EventArgs>
+    {
+        private PlanetsUpdatedWeakEventManager() { }
+
+        protected override void StartListening(object source) =>
+            ((FiltersEvent)source).PlanetsUpdated += OnItemsUpdated;
+
+        protected override void StopListening(object source) =>
+            ((FiltersEvent)source).PlanetsUpdated -= OnItemsUpdated;
+    }
+
+    #endregion
+
 }
