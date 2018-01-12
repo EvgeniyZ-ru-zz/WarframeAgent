502d975b0292fa9e7270412e136fdb5d35765b88
VladD <VladD@TIGGER>
2018-01-12 01:00:44 +0100

    Добавил примитивное отображение для проверки

--- Agent/Agent/ViewModel/ItemsViewModel.cs
+++ Agent/Agent/ViewModel/ItemsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace Agent.ViewModel
     class ItemsViewModel : VM
     {
         private GameViewModel GameView;
+
+        public ObservableCollection<ItemViewModel> Items => GameView.Items;
 
         public ItemsViewModel(GameViewModel gameView)
         {
