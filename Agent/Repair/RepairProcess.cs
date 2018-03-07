fad5224c1a14bfedda2d5260bb843310232c2e79
GodMar <stepsis2009@live.com>
2018-03-07 21:09:32 +0300

    [черновик] Дописал основной функционал утилиты обновления.

--- Agent/Repair/RepairProcess.cs
+++ Agent/Repair/RepairProcess.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Repair
 {
@@ -47,7 +50,8 @@ namespace Repair
 
         private async Task StartRepair()
         {
-            var webList = await Tools.Network.ReadTextAsync(new Uri("https://evgeniy-z.ru/files.txt"));
+            List<string> coruptFiles = new List<string>();
+            var webList = await Tools.Network.ReadTextAsync(new Uri("https://evgeniy-z.ru/files.txt?=5"));
             var ss = webList.Split(';');
             var serverFiles = (from webFile in ss
                 where !string.IsNullOrWhiteSpace(webFile)
@@ -55,14 +59,70 @@ namespace Repair
                 into array
                 select new FileList
                 {
-                    Path = array[0].Replace("\r\n", null),
+                    Path = PathCombine(Directory.GetCurrentDirectory(), array[0].Replace("\r\n", null)),
                     Md5 = array[1],
                     Size = Convert.ToInt64(array[2])
                 }).ToList();
 
 
+            foreach (var serverFile in serverFiles)
+            {
+                if (CheckFile(serverFile))
+                {
+                    coruptFiles.Add(serverFile.Path.Replace(Directory.GetCurrentDirectory(), ""));
+                }
+            }
+
+            if (coruptFiles.Any())
+            {
+                if (Directory.Exists("tempDir")) Directory.Delete("tempDir", true);
+                if (File.Exists("tempFiles")) File.Delete("tempFiles");
+                Tools.Network.DownloadFile(new Uri("https://evgeniy-z.ru/wagent/download/release"), "tempFiles" );
+                System.IO.Compression.ZipFile.ExtractToDirectory("tempFiles", "tempDir");
+
+                foreach (var coruptFile in coruptFiles)
+                {
+                    var sourceFile = PathCombine(Directory.GetCurrentDirectory(), coruptFile);
+                    var originFile = PathCombine(Path.Combine(Directory.GetCurrentDirectory(), "tempDir"), coruptFile);
+                    var fileDirectory = Path.GetDirectoryName(sourceFile);
 
 
+                    if (File.Exists(sourceFile)) File.Delete(sourceFile);
+
+                    if (!Directory.Exists(fileDirectory)) Directory.CreateDirectory(fileDirectory);
+
+                    File.Move(originFile, sourceFile);
+                }
+            }
+
+            if (Directory.Exists("tempDir")) Directory.Delete("tempDir", true);
+            if (File.Exists("tempFiles")) File.Delete("tempFiles");
+
+            Process.Start(PathCombine(Directory.GetCurrentDirectory(), "Agent.exe"));
+            Application.Current.Shutdown();
+
+        }
+
+        private bool CheckFile(FileList file)
+        {
+            var fileinfo = new FileInfo(file.Path);
+            if (!File.Exists(file.Path)) return true;
+            if (Tools.GetMd5(file.Path) != file.Md5) return true;
+            if (fileinfo.Length != file.Size) return true;
+
+            return false;
+
+        }
+
+        private string PathCombine(string path1, string path2)
+        {
+            if (Path.IsPathRooted(path2))
+            {
+                path2 = path2.TrimStart(Path.DirectorySeparatorChar);
+                path2 = path2.TrimStart(Path.AltDirectorySeparatorChar);
+            }
+
+            return Path.Combine(path1, path2);
         }
 
         private void Md5()
@@ -72,13 +132,18 @@ namespace Repair
 
             foreach (var file in files)
             {
+                if (file.Contains("Repair.exe")) continue;
                 var fileinfo = new FileInfo(file);
                 var md5 = Tools.GetMd5(file);
                 var path = file.Replace(Directory.GetCurrentDirectory(), "");
-                fileList.Add($"{path}|{md5}|{fileinfo.Length};");
+                var compliteFile = $"{path}|{md5}|{fileinfo.Length};";
+                fileList.Add(compliteFile);
+                Console.WriteLine(compliteFile);
             }
 
             File.WriteAllLines("files.list", fileList);
+
+            Application.Current.Shutdown();
         }
     }
 }
