ab0b9af5a5634ac553e7924d64ed5d393229886f
VladD <VladD@TIGGER>
2018-03-26 01:35:16 +0200

    Добавил черновой вариант toast-нотификаций

---
+++ Agent/Agent/ToastManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Agent.ViewModel;
+using Agent.View;
+
+namespace Agent
+{
+    public class ToastManager
+    {
+        ObservableCollection<ToastViewModel> toasts = new ObservableCollection<ToastViewModel>();
+
+        HashSet<ToastViewModel> toastsOnHold = new HashSet<ToastViewModel>();
+        HashSet<ToastViewModel> zombies = new HashSet<ToastViewModel>(); // on hold and removed
+
+        ToastWindow window;
+
+        public void AddToast(ToastViewModel toast)
+        {
+            zombies.Remove(toast);
+            if (!toasts.Contains(toast))
+                toasts.Add(toast);
+            if (toasts.Count > 0)
+                OpenToastWindow();
+        }
+
+        public async void AddToastWithTimeout(ToastViewModel toast, TimeSpan timeout)
+        {
+            AddToast(toast);
+            await Task.Delay(timeout);
+            RemoveToast(toast);
+        }
+
+        public void RemoveToast(ToastViewModel toast)
+        {
+            if (toastsOnHold.Contains(toast))
+                zombies.Add(toast);
+            else
+            {
+                if (toasts.Count == 1)
+                    CloseToastWindow();
+                toasts = new ObservableCollection<ToastViewModel>();
+            }
+        }
+
+        public void HoldToast(ToastViewModel toast)
+        {
+            toastsOnHold.Add(toast);
+        }
+
+        public void UnholdToast(ToastViewModel toast)
+        {
+            toastsOnHold.Remove(toast);
+            if (zombies.Remove(toast))
+                toasts.Remove(toast);
+        }
+
+        void OpenToastWindow()
+        {
+            window = new ToastWindow() { DataContext = new ReadOnlyObservableCollection<ToastViewModel>(toasts) };
+            window.Show();
+        }
+
+        void CloseToastWindow()
+        {
+            window.Close();
+            window = null;
+        }
+    }
+}
---
+++ Agent/Agent/ViewModel/ToastViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Core.ViewModel;
+
+namespace Agent.ViewModel
+{
+    public class ToastViewModel : VM
+    {
+        public ToastViewModel(string caption, string text, IReadOnlyCollection<ToastCommand> commands)
+        {
+            Caption = caption;
+            Text = text;
+            Commands = commands ?? throw new ArgumentNullException(nameof(commands));
+        }
+
+        public ToastViewModel(string caption, string text, params ToastCommand[] commands) :
+            this(caption, text, commands.ToList().AsReadOnly())
+        {
+        }
+
+        public string Caption { get; }
+        public string Text { get; }
+        // icon?
+        public IReadOnlyCollection<ToastCommand> Commands { get; }
+    }
+
+    public class ToastCommand : VM
+    {
+        public ToastCommand(ICommand command, string name)
+        {
+            Command = command ?? throw new ArgumentNullException(nameof(command));
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        public ICommand Command { get; }
+        public string Name { get; }
+    }
+}
